9d1bcd69e35f9dcc8dbaeebab388109785676c9f
Michael Thaler <michael.thaler@conesphere.com>
2019-04-04 18:18:26 +0200

    now checks for commits to master branch and pushes to origin master

--- syncallgit.sh
+++ syncallgit.sh
@@ -16,7 +16,7 @@ do
  for FOLDER in $(find $ELEMENT -maxdepth 1 -type d ); do
   if [ -d $FOLDER/.git ]; then
    echo "Git folder found in:" $FOLDER
-   ( cd $FOLDER; git pull; if [[ $(git diff --stat) != '' ]]; then echo "Uncommitted local changes found. Starting commit and push "; git commit -a; git push ; fi;   )
+   ( cd $FOLDER; git pull; if [[ $(git diff --stat) != '' ]]; then echo "Uncommitted local changes found. Starting commit and push "; git commit -a; git push ; fi;     if [[ $(git diff origin/master master)  ]]; then echo "Unpushed commits to master branch found. Starting push."; git push origin master; fi  )
   fi
  done
 done
