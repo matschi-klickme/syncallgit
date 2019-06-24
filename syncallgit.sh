2e80dc14bb1f52f7d4132c2b664b3b24072f6b10
Michael Thaler <michael.thaler@conesphere.com>
2019-06-24 13:51:58 +0200

    shellchecked script - tried some error avoidance hardeneing

--- syncallgit.sh
+++ syncallgit.sh
@@ -2,7 +2,7 @@
 #check for configfile
 if [ -f ~/.config/git_dirs ]
 then
-LOOK_DIRS=$( while read line; do echo  $line ;done  <  ~/.config/git_dirs )
+LOOK_DIRS=$( while read -r line; do echo "$line" ;done  <  ~/.config/git_dirs )
 else
 LOOK_DIRS=${1:-$PWD}
 fi
@@ -12,11 +12,27 @@ fi
 
 for ELEMENT in $LOOK_DIRS
 do
- echo "Checking folder:" $ELEMENT
- for FOLDER in $(find $ELEMENT -maxdepth 1 -type d ); do
-  if [ -d $FOLDER/.git ]; then
-   echo "Git folder found in:" $FOLDER
-   ( cd $FOLDER; git pull; if [[ $(git diff --stat) != '' ]]; then echo "Uncommitted local changes found. Starting commit and push "; git commit -a; git push ; fi;     if [[ $(git diff origin/master master)  ]]; then echo "Unpushed commits to master branch found. Starting push."; git push origin master; fi  )
-  fi
- done
+ 	echo "Checking folder:" "$ELEMENT"
+#	for FOLDER in $(find "$ELEMENT" -maxdepth 1 -type d ); do
+	(find "$ELEMENT" -maxdepth 1 -type d ) | while read -r FOLDER
+	do
+	if [ -d "$FOLDER"/.git ]; then
+	   echo "Git folder found in:" "$FOLDER"
+		( 
+	   	cd "$FOLDER" 
+		git pull 
+		if [[ $(git diff --stat) != '' ]]
+		then 
+		  echo "Uncommitted local changes found. Starting commit and push "
+		  git commit -a; git push 
+		fi
+		
+		if [[ $(git diff origin/master master)  ]]
+			then 
+				echo "Unpushed commits to master branch found. Starting push."
+				git push origin master
+	  	fi
+		)
+	  fi
+	done
 done
