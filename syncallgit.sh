2a0e23b40c931e5265b58c873e6de96232747b99
Michael Thaler <michael.thaler@conesphere.com>
2019-06-24 16:38:46 +0200

    #On branch master #Your branch is up to date with 'origin/master'. # #Changes to be committed: #  (use "git reset HEAD <file>..." to unstage) # #	modified:   syncallgit.sh # #Untracked files: #  (use "git add <file>..." to include in what will be committed) # #	.syncallgit.sh.swp #

--- syncallgit.sh
+++ syncallgit.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
-#check for configfile
-if [ -f ~/.config/git_dirs ]
+echo "Checking for configfiles"
+
+if [ -f ~/.config/syncallgit_editor_cmd ]
 then
-LOOK_DIRS=$( while read -r line; do echo "$line" ;done  <  ~/.config/git_dirs )
+	SYNCALLGIT_EDITOR_CMD="$(cat ~/.config/syncallgit_editor_cmd)" 
 else
-LOOK_DIRS=${1:-$PWD}
+	echo "no .config/syncallgit_editor_cmd file found. Using xterm -e vim as a default"
+	SYNCALLGIT_EDITOR_CMD="xterm -e vim"
 fi
 
-#check wether folder is a git repo
+if [ -f ~/.config/git_dirs ]
+then
+	CONF_LOOK_DIRS=$( while read -r line; do echo "$line" ;done  <  ~/.config/git_dirs )
+	echo ".config/git_dirs Config file found."
+fi
+LOOK_DIRS=${1:-$CONF_LOOK_DIRS}
 
+echo ""; echo "Looking for git folders in:"
+echo "$LOOK_DIRS"
 
 for ELEMENT in $LOOK_DIRS
 do
- 	echo "Checking folder:" "$ELEMENT"
+ 	echo ""; echo "Checking folder:" "$ELEMENT"
 #	for FOLDER in $(find "$ELEMENT" -maxdepth 1 -type d ); do
 	(find "$ELEMENT" -maxdepth 1 -type d ) | while read -r FOLDER
 	do
 	if [ -d "$FOLDER"/.git ]; then
+	(
 	   echo "Git folder found in:" "$FOLDER"
-		( 
-	   	cd "$FOLDER" 
+	   	cd "$FOLDER" || return 
 		git pull 
-		if [[ $(git diff --stat) != '' ]]
-		then 
-		  echo "Uncommitted local changes found. Starting commit and push "
-		  git commit -a; git push 
+		if [ "$(git diff --stat)" != '' ]
+		then
+			echo "Uncommitted local changes found. Starting commit and push "
+			#THIS IS A WORKAROUND TO THE FOLLOWING ISSUE WITH "git commit -a" WHEN USING SIGNED COMMITS
+
+			#hint: Waiting for your editor to close the file... Vim: Warning: Input is not from a terminal
+			#Vim: Error reading input, exiting.
+
+			git commit -a --dry-run > .git/COMMIT_EDITMSG; sed -i -e 's/^/#/' .git/COMMIT_EDITMSG
+			$SYNCALLGIT_EDITOR_CMD .git/COMMIT_EDITMSG  
+			git commit -a -F .git/COMMIT_EDITMSG; git push 
 		fi
-		
-		if [[ $(git diff origin/master master)  ]]
-			then 
-				echo "Unpushed commits to master branch found. Starting push."
-				git push origin master
+		if [ "$(git diff origin/master master)" != ''  ]
+		then 
+				echo "Unpushed local commits to master branch found. Starting push."
+				(git push origin master)
 	  	fi
-		)
-	  fi
+	)
+	fi
 	done
 done
